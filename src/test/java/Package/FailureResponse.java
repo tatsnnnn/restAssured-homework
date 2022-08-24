06eb99083f332e742adf5dac8fd252a6422434a5
tatsnnnn <tatsnnn@gmail.com>
2022-08-24 05:26:27 -0800

    updated files for objectmapping

---
+++ src/test/java/Package/FailureResponse.java
@@ -0,0 +1,10 @@
+package Package;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class FailureResponse {
+
+    public String error;
+}
---
+++ src/test/java/Package/RegistrationFields.java
@@ -0,0 +1,19 @@
+package Package;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.Date;
+
+@JsonPropertyOrder(alphabetic = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class RegistrationFields {
+    @JsonProperty("name")
+    public String Firstname;
+    @JsonProperty("job")
+    public String Work;
+    @JsonProperty("id")
+    public String uniqueid;
+    @JsonProperty("createdAt")
+    public Date dateCreated;
+}
